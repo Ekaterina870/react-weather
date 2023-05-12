3d1810f82dcff16864699599d75abeb2f9eddd62
Ekaterina870 <122622907+Ekaterina870@users.noreply.github.com>
2023-05-12 23:03:37 +0100

    GitHub repo URL added

--- src/App.js
+++ src/App.js
@@ -2,5 +2,19 @@ import React from "react";
 import "./App.css";
 
 export default function App() {
-  return <div className="App">Weather App</div>;
+  return (
+    <div className="App">
+      <h1> Weather App</h1>
+      <footer>
+        This project was coded by Ekaterina Volkova{" "}
+        <a
+          href="https://github.com/Ekaterina870/react-weather"
+          target="_blank"
+          rel="noreferrer"
+        >
+          and is open-sourced on GitHub
+        </a>
+      </footer>
+    </div>
+  );
 }
