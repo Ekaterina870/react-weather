15c5ff9904350e4648cb1445e9783c2487510a54
Ekaterina870 <122622907+Ekaterina870@users.noreply.github.com>
2023-05-13 16:04:04 +0100

    Weather.css basic styling

--- src/Weather.js
+++ src/Weather.js
@@ -7,34 +7,33 @@ export default function Weather() {
       <h1>Find a forecast</h1>
       <br />
       <form>
-        <input type="search" placeholder="Enter a City Name" />
+        <input type="search" placeholder="Enter a City Name" autoFocus="on" />
 
         <input type="submit" value="Search" />
         <input type="submit" value="Current" />
       </form>
       <br />
       <div className="row">
-        <div className="col-6">
-          <div className="row">
-            <div className="col-6">
-              <ul>
-                <li>Last updated:</li>
-                <li>Day & Time</li>
-                <li>Description</li>
-              </ul>
-            </div>
-            <div className="col-6">
-              <h2>City</h2>
-              12°C
-              <img
-                src="https://easydrawingguides.com/wp-content/uploads/2017/06/how-to-draw-a-sun-featured-image-1200.png"
-                width="20%"
-                alt="Sunny weather"
-              ></img>
-            </div>
-          </div>
+        <div className="col-4">
+          {" "}
+          <h2>City</h2>
+          <span className="temperature">12</span>
+          <span className="units">°C</span>
+          <img
+            src="https://easydrawingguides.com/wp-content/uploads/2017/06/how-to-draw-a-sun-featured-image-1200.png"
+            width="20%"
+            alt="Sunny weather"
+          ></img>
         </div>
-        <div className="col-6">
+        <div className="col-4">
+          <ul>
+            <li>Last updated:</li>
+            <li>Day & Time</li>
+            <li>Description</li>
+          </ul>
+        </div>
+
+        <div className="col-4">
           <ul>
             <li>Feels like:12°C</li>
             <li>Humidity:%</li>
