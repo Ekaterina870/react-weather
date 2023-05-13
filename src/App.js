96f5366847b180965f0d01e026192e1aa10ab514
Ekaterina870 <122622907+Ekaterina870@users.noreply.github.com>
2023-05-13 14:37:17 +0100

    Bootstrap added

--- src/App.js
+++ src/App.js
@@ -4,17 +4,20 @@ import "./App.css";
 export default function App() {
   return (
     <div className="App">
-      <h1> Weather App</h1>
-      <footer>
-        This project was coded by Ekaterina Volkova{" "}
-        <a
-          href="https://github.com/Ekaterina870/react-weather"
-          target="_blank"
-          rel="noreferrer"
-        >
-          and is open-sourced on GitHub
-        </a>
-      </footer>
+      <div className="container">
+        <h1> Weather App</h1>
+        <button className="btn btn-primary">Hello</button>
+        <footer>
+          This project was coded by Ekaterina Volkova{" "}
+          <a
+            href="https://github.com/Ekaterina870/react-weather"
+            target="_blank"
+            rel="noreferrer"
+          >
+            and is open-sourced on GitHub
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
